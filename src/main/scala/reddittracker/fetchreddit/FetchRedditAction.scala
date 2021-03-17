0edfbe4ef30327939a37a7b85d1f782a82cb5dd8
Brighton Balfrey <balfrey@usc.edu>
2021-03-16 17:26:35 -0700

    added action to fetch reddit

--- src/main/scala/reddittracker/fetchreddit/FetchRedditAction.scala
+++ src/main/scala/reddittracker/fetchreddit/FetchRedditAction.scala
@@ -1,9 +1,48 @@
 package com.brighton1101.reddittracker.fetchreddit
 
-import com.brighton1101.reddittracker.common.CliAction
+import java.io.{File, PrintWriter}
+
+import com.brighton1101.reddittracker.common.client.{RedditClient, RedditClientImpl}
+import com.brighton1101.reddittracker.common.{CliAction, HttpClient, Json, SttpHttpClient}
+
+import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.util.{Failure, Success}
+
+case class FetchRedditConfig(
+                            resultsDestination: String,
+                            subreddit: String,
+                            after: Option[String],
+                            before: Option[String],
+                            limit: Option[Int])
+
 
 class FetchRedditAction extends CliAction {
+
+  lazy val httpClient: HttpClient = SttpHttpClient.getClient
+  lazy val redditClient: RedditClient = new RedditClientImpl(httpClient)
+
   def run(args: Seq[String]): Unit = {
-    args.foreach(println(_))
+    if (args.size < 1) return
+    val fileSrc = scala.io.Source.fromFile(args(0))
+    val config = Json.fromJson[FetchRedditConfig](fileSrc.mkString)
+    fileSrc.close
+    val res = redditClient.getSubredditPosts(config.subreddit, config.after, config.before, config.limit)
+      .map(_.map { posts =>
+        /** Terrible code here but wanted to do it quickly */
+        val writer = new PrintWriter(new File(config.resultsDestination))
+        writer.write(Json.toDelimJson(posts))
+        writer.close()
+        ()
+      })
+    res onComplete {
+      case Success(Right(_)) => println("Success")
+      case _ => {
+        println("Failure")
+        java.lang.System.exit(1)
+      }
+    }
+    Await.ready(res, Duration.Inf)
   }
 }
